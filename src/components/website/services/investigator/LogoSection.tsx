84a4e2578c36f4e5af4409139b4a73c43eb2721c
harish-backend-dev <harish.backend.dev@gmail.com>
2026-08-17 12:58:17 +0530

    Update LogoSection.tsx

--- src/components/website/services/investigator/LogoSection.tsx
+++ src/components/website/services/investigator/LogoSection.tsx
@@ -3,37 +3,41 @@ import React from "react";
 
 interface LogoSectionProps {
   title?: string;
-  subtitle?: string;
   logos?: { src: string; alt: string }[];
 }
 
 const LogoSection: React.FC<LogoSectionProps> = ({
-  title = "Our Compliance & Accreditation",
-  subtitle = "Rumax platform meets the highest certification standards for data security & privacy in healthcare, leveraging industry standards to secure data for our clients.",
+  title = "Our Compliance & accreditation",
+  desc = "Rumax platform meets the highest certification standards for data security & privacy in healthcare, leveraging industry standards to secure data for our clients.",
   logos = [
-    { src: "ceca.png", alt: "CECA" },
-    { src: "cqc-logo.png", alt: "Care Quality Commission" },
+    { src: "eca.png", alt: "ECA" },
+    { src: "cqc-logo.png", alt: "CQC" },
     { src: "nhs.png", alt: "NHS" },
     { src: "ico.png", alt: "ICO" },
-    { src: "iso-9001.png", alt: "ISO 9001:2015" },
+    { src: "iso.png", alt: "ISO" },
+    { src: "cyber-essential.png", alt: "Cyber Essential" },
   ],
 }) => {
   return (
-    <section className="compliance-section">
+    <div className="logo-main">
       <div className="container">
-        <div className="compliance-header">
-          <h2>{title}</h2>
-          <p>{subtitle}</p>
-        </div>
-        <div className="compliance-logos">
-          {logos.map((logo, index) => (
-            <div className="logo-item" key={index}>
-              <img src={logo.src} alt={logo.alt} />
+        <div className="inner-logos">
+          <div className="left-logo-0">
+            <h3>{title}</h3>
+              <p>{desc}</p>
+          </div>
+          <div className="right-logo-0">
+            <div className="footer-certificates">
+              {logos.map((logo, index) => (
+                <div className="logo-foot" key={index}>
+                  <img src={logo.src} alt={logo.alt} />
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
